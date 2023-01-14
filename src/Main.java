9f797da096a37495099f18a0a1a15dd2811c548f
LexHeadko <n-odes11rus@yandex.ru>
2023-01-14 15:39:57 +0200

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        //task3();
+        //task4();
+        //task5();
+        //task6();
+        //task7();
+        //task8();
     }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        byte per1 = 8;
+        short per2 = 3;
+        int per3 = 763789;
+        long per4 = 789874564L;
+        float per5 = 3.25f;
+        double per6 = 3.231654354626149;
+        System.out.println(
+                "Значение переменной byte равно " + per1 + "," +
+                        "Значение переменной short равно " + per2 + "," +
+                        "Значение переменной int равно " + per3 + "," +
+                        "Значение переменной long равно " + per4 + "," +
+                        "Значение переменной float равно " + per5 + "," +
+                        "Значение переменной double равно " + per6 + ".");
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        float per1 = 27.12f;
+        long per2 = 987678965549L;
+        float per3 = 2.786f;
+        short per4 = 569;
+        short per5 = -159;
+        short per6 = 27897;
+        byte per7 = 67;
+    }
+
 }
